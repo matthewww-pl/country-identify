0b0f67d5c458d5f7d99acd6a0a346c738f67b4a1
Mateusz Błaszczak <mateusz.blaszczak@jastkow.pl>
2020-12-18 14:14:41 +0100

    Create Home (head) template

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,66 @@
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+  <div>
+    <b-container >
+      <b-jumbotron>
+        <template #header>Country Identify App</template>
+        <template #lead>
+          Application to display information about countries from around the world. Check it out.
+        </template>
+        <hr class="my-4">
+        <b-row>
+          <b-col cols="6">
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+              Search your country on bottom <b-icon icon="arrow-down" animation="cylon-vertical" font-scale="2"></b-icon>
+              <b-form-group
+                id="input-group-1"
+                label-for="input-1"
+                description="Allow uppercase and lowercase"
+                class="mt-2"
+              >
+                <b-form-input
+                  id="input-1"
+                  v-model="form.name"
+                  type="text"
+                  placeholder="Enter Country"
+                  required
+                ></b-form-input>
+              </b-form-group>
+              <b-button type="submit" variant="success">Search</b-button>
+              <b-button class="ml-2" type="reset" variant="danger">Reset</b-button>
+            </b-form>
+          </b-col>
+        </b-row>
+        <hr class="my-4">
+      </b-jumbotron>
+    </b-container>
+  </div>
 </template>
 
 <script>
 export default {
-    
+  data() {
+    return {
+      form: {
+        name: ''
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(event) {
+      event.preventDefault()
+      this.form.name = ''
+    }
+  }
 }
 </script>
+
+<style>
+  body{
+    background-color: #292222;
+  }
+</style>
