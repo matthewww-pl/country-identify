a3b71877869e5e89155be0373f0fbe39b8ba59f9
Mateusz Błaszczak <mateusz.blaszczak@jastkow.pl>
2020-12-18 15:21:52 +0100

    Add new elements on layout, connect to countries api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
         </template>
         <hr class="my-4">
         <b-row>
-          <b-col cols="6">
+          <b-col sm="12" md="6">
             <b-form @submit="onSubmit" @reset="onReset" v-if="show">
               Search your country on bottom <b-icon icon="arrow-down" animation="cylon-vertical" font-scale="2"></b-icon>
               <b-form-group
@@ -31,29 +31,68 @@
           </b-col>
         </b-row>
         <hr class="my-4">
+        <b-row v-if="data.length!==0">
+          <b-col sm="12" md="6">
+            <b-table striped hover :items="data"></b-table>
+          </b-col>
+          <b-col sm="12" md="6">
+            <h5>Flag</h5>
+            <b-img class="mt-2" :src="flag" fluid alt="Flag"/>
+          </b-col>
+        </b-row>
       </b-jumbotron>
     </b-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
       form: {
         name: ''
       },
-      show: true
+      show: true,
+      fields: ['Param', 'Info'],
+      data: [],
+      flag: null,
     }
   },
   methods: {
     onSubmit(event) {
       event.preventDefault()
-      alert(JSON.stringify(this.form))
+      this.data = []
+      this.flag = null
+      axios.get('https://restcountries.eu/rest/v2/name/'+this.form.name).then(response => {
+        this.flag = response.data[0].flag
+        this.data.push({'param': 'Name', 'info': response.data[0].name})
+        this.data.push({'param': 'Capital', 'info': response.data[0].capital})
+        this.data.push({'param': 'Language', 'info': response.data[0].languages[0].name})
+        this.data.push({'param': 'Alpha 2 Code', 'info': response.data[0].alpha2Code})
+        this.data.push({'param': 'Alpha 3 Code', 'info': response.data[0].alpha3Code})
+        this.data.push({'param': 'Population', 'info': response.data[0].population})
+        this.data.push({'param': 'Calling Code', 'info': response.data[0].callingCodes[0]})
+        this.data.push({'param': 'Region', 'info': response.data[0].region})
+      }).catch(error => {
+        if(error.response.status===404){
+          this.makeToast('b-toaster-top-center','danger')
+        }
+      })
     },
     onReset(event) {
       event.preventDefault()
       this.form.name = ''
+      this.data = []
+      this.flag = null
+    },
+    makeToast(toaster, variant = null) {
+      this.$bvToast.toast('Please enter a country name', {
+        title: `Error`,
+        toaster: toaster,
+        variant: variant,
+        solid: true
+      })
     }
   }
 }
